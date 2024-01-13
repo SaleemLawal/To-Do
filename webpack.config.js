ac658f27f51529bffdfd6ff405aad1f8753001af
SaleemLawal <slawal1@umbc.edu>
2024-01-13 13:34:09 -0500

    config file for webpack

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/scripts/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    devServer:{
+        static: './dist',
+        port: 8080,
+        watchFiles: ['src/**/*.html', 'src/**/*.css', 'src/**/*.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader','css-loader',
+                ]
+            },
+            {
+                test:/\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[hash].[ext]',
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    'html-loader'
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
+
+}
