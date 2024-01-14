c4e98fa61d2c7d5c23708657e170d8a0f52292ae
SaleemLawal <slawal1@umbc.edu>
2024-01-14 18:34:05 -0500

    JS for making projects objects

--- src/scripts/project.js
+++ src/scripts/project.js
@@ -1,3 +1,32 @@
 // Defines a Project class to represent projects or separate lists of to-dos.
 // Manages the collection of to-dos within a project.
 // Includes methods for adding, updating, and deleting to-dos within the project.
+class Project{
+    constructor (title, description){
+        this.title = title
+        this.description = description
+        this.todos = []
+    }
+    addTodo (todo){
+        this.todos.push(todo)
+    }
+    removeTodo (todo){
+        this.todos.splice(this.todos.indexOf(todo), 1)
+    }
+    editTitle(title){
+        this.title = title
+    }
+    editDescription(description){
+        this.description = description
+    }
+    getTitle(){
+        return this.title
+    }
+    getTodos(){
+        return this.todos
+    }
+    getDescription(){
+        return this.description
+    }
+}
+export {Project}
