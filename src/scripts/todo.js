b3c93e06dd63f049be6541b3d05de70cd23b16f0
SaleemLawal <slawal1@umbc.edu>
2024-01-14 18:33:38 -0500

    added JS to make Todo Objects

--- src/scripts/todo.js
+++ src/scripts/todo.js
@@ -1,2 +1,26 @@
 // Defines a Todo class to represent individual to-do items.
-// Includes properties like title, description, dueDate, priority, notes, and checklist.
+// Includes properties like title, dueDate, priority, notes, and checklist.
+// Also includes methods to edit any of these properties.
+
+export default function createTodo(title, dueDate, priority){
+    const makeTodo = () => {
+        let title = title
+        let dueDate = dueDate
+        let priority = priority
+        return {title, dueDate, priority}
+    }
+    const editTitle = (newTitle) => {
+        title = newTitle
+    }
+    const editDueDate = (newDueDate) => {
+        dueDate = newDueDate
+    }
+    const editPriority = (newPriority) => {
+        priority = newPriority
+    }
+    const getTitle = () => title
+    const getDueDate = () => dueDate
+    const getPriority = () => priority
+
+    return {makeTodo, editTitle, editDueDate, editPriority, getTitle, getDueDate, getPriority}
+}
