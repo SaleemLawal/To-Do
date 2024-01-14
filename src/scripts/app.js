3212f4d88665f553ff9072a9d63405d946f20981
SaleemLawal <slawal1@umbc.edu>
2024-01-14 18:35:06 -0500

    added some test calls

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,9 @@
 // Acts as the main application module.
 // Coordinates interactions between different parts of the application.
 // Initializes event listeners and sets up the overall application structure.
+
+import normalize from '../stylesheet/normalize.css'
+import style from  '../stylesheet/style.css'
+
+import { getAction } from './ui.js';
+getAction();
