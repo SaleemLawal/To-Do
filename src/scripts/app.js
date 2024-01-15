7c6175710082c3bcdebc878499d1a281e8bfd3fe
SaleemLawal <slawal1@umbc.edu>
2024-01-15 11:56:36 -0500

    edited initApp function for event listener initialization

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,26 +5,15 @@
 import normalize from '../stylesheet/normalize.css'
 import style from  '../stylesheet/style.css'
 import getAction from './ui.js';
-import { createNewProject, createNewTodo } from './form.js';
+import { projectFunction, todoFunction } from './form.js';
 
 // Function to initialize the application
 function initApp() {
-    // const createProject = document.querySelector('[data-create-project-btn]');
-    // // need to add function that creates a new project
-    // createProject.addEventListener('click', );
-
-    // data action buttons
-    // const actionButtons = document.querySelector('[data-action-buttons]');
-
-    // toggle between all todo and important todo
-    // const toggle = document.querySelector('[data-btn-toggle]');
-    // toggle.addEventListener('click', );
-
-    // create todo
+    const createProject = document.querySelector('[data-create-project-btn]');
     const createTodo = document.querySelector('[data-create-todo]');
-
-    createTodo.addEventListener('click', createNewTodo);
-
+    // need to add function that creates a new project and todo when the buttons are clicked
+    createProject.addEventListener('click', projectFunction);
+    createTodo.addEventListener('click', todoFunction);
 }
 initApp();
 getAction();
