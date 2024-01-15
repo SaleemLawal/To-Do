b47660bc7b3cca7e72d032b5614bc7f5c42e014c
SaleemLawal <slawal1@umbc.edu>
2024-01-14 23:01:42 -0500

    added functions that generates content in html when generate forms/projects is clicked

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -4,7 +4,7 @@ function GenerateForm(){
     // form creation 
     const newForm = document.createElement('form');
     newForm.setAttribute('id', 'new-form');
-    newForm.setAttribute('class', 'form');
+    newForm.setAttribute('class', 'form-container');
     newForm.setAttribute('action', '#');
     newForm.setAttribute('method', 'post');
 
@@ -13,9 +13,9 @@ function GenerateForm(){
     formTitle.setAttribute('class', 'form-title');
     formTitle.textContent = 'Create New Todo';
 
-    // form input fields
-    const inputFields = document.createElement('div');
-    inputFields.setAttribute('class', 'input-fields');
+    // Name Div
+    const nameDiv = document.createElement('div');
+    nameDiv.setAttribute('class', 'input-fields');
     // name
     const nameLabel = document.createElement('label');
     nameLabel.setAttribute('for', 'name');
@@ -23,9 +23,14 @@ function GenerateForm(){
 
     const inputName = document.createElement('input');
     inputName.setAttribute('id', 'todo-name');
-    nameLabel.appendChild(inputName);
+    
+    nameDiv.appendChild(nameLabel);
+    nameDiv.appendChild(inputName);
 
     // date
+    const dateDiv = document.createElement('div');
+    dateDiv.setAttribute('class', 'input-fields');
+
     const dateLabel = document.createElement('label');
     dateLabel.setAttribute('for', 'date');
     dateLabel.textContent = 'Date';
@@ -34,9 +39,14 @@ function GenerateForm(){
     inputDate.setAttribute('id', 'todo-date');
     inputDate.setAttribute('type', 'date');
     inputDate.setAttribute('placeholder', 'mm/dd/yyyy');
-    dateLabel.appendChild(inputDate);
+
+    dateDiv.appendChild(dateLabel);
+    dateDiv.appendChild(inputDate);
 
     // priority
+    const priorityDiv = document.createElement('div');
+    priorityDiv.classList.add('priority');
+
     const priorityLabel = document.createElement('label');
     priorityLabel.setAttribute('for', 'priority');
     priorityLabel.textContent = 'Important?';
@@ -45,21 +55,31 @@ function GenerateForm(){
     inputPriority.setAttribute('type', 'checkbox');
     inputPriority.setAttribute('id', 'todo-priority');
 
-    priorityLabel.appendChild(inputPriority);
+
+    priorityDiv.appendChild(priorityLabel);
+    priorityDiv.appendChild(inputPriority);
 
     // form submission
     const submitButton = document.createElement('button');
     submitButton.setAttribute('type', 'submit');
-    submitButton.setAttribute('id', 'todo-submit');
+    submitButton.setAttribute('data-btn-submit', '');
     submitButton.textContent = 'Submit';
 
-    inputFields.appendChild(nameLabel);
-    inputFields.appendChild(dateLabel);
-    inputFields.appendChild(priorityLabel);
+    const buttonClose = document.createElement('button');
+    buttonClose.setAttribute('type', 'button');
+    buttonClose.setAttribute('class', 'btn-close');
+    buttonClose.setAttribute('data-btn-close', '');
+    buttonClose.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="24px" height="24px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="m12 13.4l-4.9 4.9q-.275.275-.7.275q-.425 0-.7-.275q-.275-.275-.275-.7q0-.425.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7q0-.425.275-.7q.275-.275.7-.275q.425 0 .7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275q.425 0 .7.275q.275.275.275.7q0 .425-.275.7L13.4 12l4.9 4.9q.275.275.275.7q0 .425-.275.7q-.275.275-.7.275q-.425 0-.7-.275Z"></path></svg>
+    `
 
+    // append all elements to the form
     newForm.appendChild(formTitle);
-    newForm.appendChild(inputFields);
+    newForm.appendChild(nameDiv);
+    newForm.appendChild(dateDiv);
+    newForm.appendChild(priorityDiv);
     newForm.appendChild(submitButton);
+    newForm.appendChild(buttonClose);
 
     return newForm;
 }
@@ -111,4 +131,58 @@ function GenerateProject(){
     newProject.appendChild(submitButton);
 }
 
-export { GenerateForm, GenerateProject };
+function eventFunctions() {
+    const createNewProject = () => {
+        // generate a blur effect on the main container
+        // show the form for creating a new project
+
+        // add event listener to the submit button
+
+        // when the submit button is clicked, create a new project object and add it to the project list
+
+        // hide the form
+
+        // remove the blur effect
+
+        // update the screen
+
+    }
+
+    const createNewTodo = () => {
+
+        // show the form for creating a new todo
+        const newForm = GenerateForm();
+        const modal = document.querySelector('.modal');
+        modal.classList.remove('display-none');
+        document.querySelector('.modal').appendChild(newForm);
+
+        // add event listener to the submit button
+        const submitButton = document.querySelector('[data-btn-submit]');
+        submitButton.addEventListener('click', () => {
+            // get the values from the form
+            const name = document.querySelector('#todo-name').value;
+            const date = document.querySelector('#todo-date').value;
+            const priority = document.querySelector('#todo-priority').checked;
+
+            // create a new todo object
+            const newTodo = TodoItem(name, date, priority);
+            // add it to the todo list
+            todoList.push(newTodo);
+            // update the screen
+            const updateScreen = renderScreen();
+            updateScreen.renderGeneral();
+        });
+
+        // add event listener to the x button
+        const xButton = document.querySelector('[data-btn-close]');
+        xButton.addEventListener('click', () => {
+            modal.classList.add('display-none');
+            document.querySelector('.modal').removeChild(newForm);
+        });
+
+    }
+    return { createNewProject, createNewTodo };
+}
+
+export const { createNewProject, createNewTodo } = eventFunctions();
+
