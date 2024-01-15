eda189decb7f24501930e08196f3e965bf25fcc0
SaleemLawal <slawal1@umbc.edu>
2024-01-14 23:03:19 -0500

    added function to init app and import from form

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,6 +4,27 @@
 
 import normalize from '../stylesheet/normalize.css'
 import style from  '../stylesheet/style.css'
+import getAction from './ui.js';
+import { createNewProject, createNewTodo } from './form.js';
 
-import { getAction } from './ui.js';
+// Function to initialize the application
+function initApp() {
+    // const createProject = document.querySelector('[data-create-project-btn]');
+    // // need to add function that creates a new project
+    // createProject.addEventListener('click', );
+
+    // data action buttons
+    // const actionButtons = document.querySelector('[data-action-buttons]');
+
+    // toggle between all todo and important todo
+    // const toggle = document.querySelector('[data-btn-toggle]');
+    // toggle.addEventListener('click', );
+
+    // create todo
+    const createTodo = document.querySelector('[data-create-todo]');
+
+    createTodo.addEventListener('click', createNewTodo);
+
+}
+initApp();
 getAction();
