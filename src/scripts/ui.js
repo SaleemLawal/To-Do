759417da5556f9e5e4010fd601fcd2ee4c6c9c3a
SaleemLawal <slawal1@umbc.edu>
2024-01-17 21:53:32 -0500

    added function that display items on the screen based on their categories

--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -1,9 +1,11 @@
-// Manages the user interface, including DOM manipulation.
-// Handles dynamic rendering of projects, to-dos, and forms.
-// Listens for user interactions (e.g., button clicks) and triggers corresponding actions.
+
+import {makeTodoItem} from './task.js';
+import Todo from './todo.js';
+import createButtonListener from './app.js';
+import { getGeneralTodos, getProject, getTodayTodos, getUpcomingTodos, getCompletedTodos, getDeletedTodos } from "./storage";
 
 // Function to handle user actions and navigate the page
-export default function getAction() {
+export default function navigation() {
     // adds event listener to the divs in the home-actions container
     const mainActions = document.querySelectorAll('[data-actions]');
     mainActions.forEach(action => {
@@ -42,6 +44,7 @@ function navigatePage(clicked){
             console.error(`Unhandled action: ${clicked}`);
             break;
     }
+    createButtonListener();
 }
 
 // Function to render different screens
@@ -52,20 +55,23 @@ function renderScreen(){
 
     const renderGeneral = () => {
         pTag.innerHTML = 'A general list of tasks';
+        // check if the action buttons are already there
         if (!getActionButtons) {
             // need to make a new div and generate the buttons, then append it before task-list in main-container div
             const actionButtons = document.createElement('div');
             actionButtons.setAttribute('data-action-buttons', '');
             actionButtons.classList.add('action-buttons');
 
-            // make the buttons
+            // make the buttons (need to add event listeners to them)
             const allButton = document.createElement('button');
             allButton.classList.add('toggle');
+            allButton.setAttribute('data-btn-toggle', '')
             allButton.textContent = 'All';
 
-            // make create to do button
+            // make create to do button (need to add event listeners to them)
             const createButton = document.createElement('button');
             createButton.classList.add('create-tasks');
+            createButton.setAttribute('data-create-todo', '');
             createButton.innerHTML = `
                 <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="24px" height="24px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M18 12.998h-5v5a1 1 0 0 1-2 0v-5H6a1 1 0 0 1 0-2h5v-5a1 1 0 0 1 2 0v5h5a1 1 0 0 1 0 2z"></path></svg>
                 Create Todo
@@ -78,23 +84,27 @@ function renderScreen(){
             mainContainer.insertBefore(actionButtons, mainContainer.querySelector('[data-task-list]'));
         }
         // Render all tasks to be done
+        displayItems(getGeneralTodos())
     }
     
     const renderToday = () =>{
         pTag.innerHTML = 'A list of tasks for today';
+        // removes the action buttons if they are there
         if (getActionButtons){
             mainContainer.removeChild(getActionButtons);
         }
          
         // render task to be done today
+        displayItems(getTodayTodos())
     }
     const renderUpcoming = () =>{
         pTag.innerHTML = 'A list of tasks for the future';
-        //  = mainContainer.querySelector('[data-action-buttons]');
+        
         if (getActionButtons){
             mainContainer.removeChild(getActionButtons);
         }
         // render task to be done in the future
+        displayItems(getUpcomingTodos())
     }
     const renderCompleted = () =>{
         pTag.innerHTML = 'A list of completed tasks';
@@ -102,6 +112,7 @@ function renderScreen(){
             mainContainer.removeChild(getActionButtons);
         }
         // render task done
+        displayItems(getCompletedTodos())
     }
     const renderTrash = () =>{
         pTag.innerHTML = 'A list of deleted tasks';
@@ -109,9 +120,19 @@ function renderScreen(){
             mainContainer.removeChild(getActionButtons);
         }
         // render task deleted
+        displayItems(getDeletedTodos())
     }
 
     return {renderGeneral, renderToday, renderUpcoming, renderCompleted, renderTrash}
 }
-
-
+function displayItems(todoContainer){
+    const todoList = document.querySelector('[data-task-list]');
+    todoList.innerHTML = '';
+    for (let i = 0; i < todoContainer.length; i++){
+        const obj = todoContainer[i]
+        const todoInstance = Todo.fromObject(obj);
+        const newTodoItem = makeTodoItem(todoInstance);
+        todoList.appendChild(newTodoItem);
+    }
+}
+export const {renderGeneral} = renderScreen();
