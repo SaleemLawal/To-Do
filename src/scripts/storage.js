225d8de0905f65b66af84bb21fa5ab80220c07a5
SaleemLawal <slawal1@umbc.edu>
2024-01-17 21:51:23 -0500

    Implemented functions that set and get storage elements for todo content containers

--- src/scripts/storage.js
+++ src/scripts/storage.js
@@ -1,2 +1,67 @@
-// Handles local storage interactions to persist projects and to-dos.
-// Provides functions to save and retrieve data from local storage.
+export default function initStorage() {
+    const categories = ['project', 'generalTodos', 'todayTodos', 'upcomingTodos', 'completedTodos', 'deletedTodos'];
+
+    categories.forEach(category => {
+        if (!localStorage[category]) {
+            localStorage[category] = JSON.stringify([]);
+        }
+    });
+}
+
+function saveTodo(category, todo) {
+    const todos = JSON.parse(localStorage[category]);
+    todos.push(todo);
+    localStorage[category] = JSON.stringify(todos);
+}
+
+function getTodos(category) {
+    return JSON.parse(localStorage[category]);
+}
+
+export function saveGeneralTodos(todo) {
+    saveTodo('generalTodos', todo);
+}
+
+export function getGeneralTodos() {
+    return getTodos('generalTodos');
+}
+
+export function saveProject(project) {
+    saveTodo('project', project);
+}
+
+export function getProject() {
+    return getTodos('project');
+}
+
+export function saveTodayTodos(todo) {
+    saveTodo('todayTodos', todo);
+}
+
+export function getTodayTodos() {
+    return getTodos('todayTodos');
+}
+
+export function saveUpcomingTodos(todo) {
+    saveTodo('upcomingTodos', todo);
+}
+
+export function getUpcomingTodos() {
+    return getTodos('upcomingTodos');
+}
+
+export function saveCompletedTodos(todo) {
+    saveTodo('completedTodos', todo);
+}
+
+export function getCompletedTodos() {
+    return getTodos('completedTodos');
+}
+
+export function saveDeletedTodos(todo) {
+    saveTodo('deletedTodos', todo);
+}
+
+export function getDeletedTodos() {
+    return getTodos('deletedTodos');
+}
